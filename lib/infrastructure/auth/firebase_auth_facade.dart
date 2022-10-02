433e6e21537a73c2e965124f02f78141fde02109
Peter Nathaniel <peternathaniel49@gmail.com>
2022-10-02 23:32:08 +0100

    injected dependencies using injectable

--- lib/infrastructure/auth/firebase_auth_facade.dart
+++ lib/infrastructure/auth/firebase_auth_facade.dart
@@ -2,10 +2,14 @@ import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:injectable/injectable.dart';
 import 'package:notepad_firebase_ddd/domain/auth/auth_failure.dart';
 import 'package:notepad_firebase_ddd/domain/auth/value_objects.dart';
 import '../../domain/auth/I_auth_facade.dart';
+import 'package:injectable/injectable.dart';
 
+
+@LazySingleton(as: IAuthFacade)
 class FirebaseAuthFacade implements IAuthFacade {
   final FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
